7cade3ed2ded11cdfe9962dbc0dc37c55d9f0990
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2020-10-14 13:35:39 +0100

    Criada classe SereieDeIngresso

---
+++ Estudantes[CodeLopp]/Models/SerieDeIngresso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Estudantes_CodeLopp_.Models
+{
+    public class SerieDeIngresso
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Este campo deve ser preenchido!")]
+        [Display(Name = "Série de Ingresso")]
+        public string Serie { get; set; }
+    }
+
+}
